5140195298ad194eb726ec9489ffef167a4a6686
Bemi Irone <bemidev@nsmultimedia.co.uk>
2024-06-05 08:45:17 +0100

    Store added

---
+++ src/SnippetStore.ts
@@ -0,0 +1,33 @@
+import { writable, get } from 'svelte/store';
+
+const snippetStore = writable<CodeSnippet[]>([]);
+
+// add snippet
+export function addSnippet(snippet: CodeSnippetInput) {
+  const newSnippet: CodeSnippet = {
+    ...snippet,
+    favorite: false
+  };
+  snippetStore.update((items) => {
+    return [newSnippet, ...items];
+  });
+} 
+
+// remove snippet
+export function removeSnippet(index: number) {
+  snippetStore.update((items) => {
+    return items.filter((_, i) => i !== index);
+  });
+}
+
+// toggle favorite
+export function toggleFavorite(index: number) {
+  snippetStore.update((items) => {
+    return items.map((snippet, i) => {
+      if (i === index) {
+        snippet.favorite = !snippet.favorite;
+      }
+      return snippet;
+    });
+  });
+}
